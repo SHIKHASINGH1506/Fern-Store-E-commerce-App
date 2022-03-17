ca9f7c965abf136c3283284d326a28a7a1250a4a
Shikha <shikhakumarisingh9715@gmail.com>
2022-03-17 20:43:02 +0530

    Typos fixed in categories

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -5,29 +5,25 @@ import {
   AirPurifierCategory
 } from "../../assets/index";
 
-/**
- * Category Database can be added here.
- * You can add category of your wish with different attributes
- * */
 
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "indoor",
+    categoryName: "Indoor",
     img: {IndoorCategory},
     description:
       "literature in the form of prose, especially novels, that describes imaginary events and people",
   },
   {
     _id: uuid(),
-    categoryName: "airPurifier",
+    categoryName: "Air Purifier",
     img: {AirPurifierCategory},
     description:
       "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
   },
   {
     _id: uuid(),
-    categoryName: "lowMaintenance",
+    categoryName: "Low Maintenance",
     img: {LowMaintainanceCategory},
     description:
       "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
