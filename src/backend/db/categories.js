6696ddadb0560cfda600c697698107b0235dedbc
Shikha <shikhakumarisingh9715@gmail.com>
2022-03-21 15:38:50 +0530

    got catrgory data from api and redndered featured category on home page

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -10,21 +10,21 @@ export const categories = [
   {
     _id: uuid(),
     categoryName: "Indoor",
-    img: {IndoorCategory},
+    img: IndoorCategory,
     description:
       "literature in the form of prose, especially novels, that describes imaginary events and people",
   },
   {
     _id: uuid(),
     categoryName: "Air Purifier",
-    img: {AirPurifierCategory},
+    img: AirPurifierCategory,
     description:
       "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
   },
   {
     _id: uuid(),
     categoryName: "Low Maintenance",
-    img: {LowMaintainanceCategory},
+    img: LowMaintainanceCategory,
     description:
       "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
   },
