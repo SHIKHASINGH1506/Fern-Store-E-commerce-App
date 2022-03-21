e7f143118c7955e67a1af88640166b9f74df53cb
Shikha <shikhakumarisingh9715@gmail.com>
2022-03-21 15:43:08 +0530

    index files added in context folder for better import and export

---
+++ src/contexts/index.js
@@ -0,0 +1,5 @@
+import { FilterContextProvider, useFilter } from "./filterContext";
+import { AuthContextProvider, useAuth } from "./authContext";
+
+export { FilterContextProvider, useFilter };
+export { AuthContextProvider, useAuth};
